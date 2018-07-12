a93db36ac303294d25b09ba760001b26dff650b2
lokeshrajarora <lokesh.slg06@gmail.com>
2018-07-13 00:02:53 +0530

    [update] add support for frontend hosting

--- src/server/app.ts
+++ src/server/app.ts
@@ -6,6 +6,9 @@ import * as mongoose from "mongoose";
 import * as settings from "settings";
 import * as morgan from "morgan";
 import * as cors from "cors";
+import * as helmet from "helmet";
+import * as compression from "compression";
+import * as path from "path";
 /**
  * Initialize the database.
  */
@@ -30,12 +33,18 @@ export default app;
  * Support json & urlencoded requests.
  */
 app.use(cors());
+app.use(helmet());
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "frontend")));
 
 require("api/users");
 require("api/text");
+app.get("*", (req: express.Request, res: express.Response) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
 /**
  * Run the server
  */
